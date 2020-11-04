2a27152518895f27e78a1a8a4f79cf998920968a
wpenglish <wpenglish27@gmail.com>
2020-11-03 20:18:41 -0500

    Added Firebase Config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAtCRHSSIwa5UepmVBQE6sPAeqI28S3XRk",
+  authDomain: "admin-portal-firebase.firebaseapp.com",
+  databaseURL: "https://admin-portal-firebase.firebaseio.com",
+  projectId: "admin-portal-firebase",
+  storageBucket: "admin-portal-firebase.appspot.com",
+  messagingSenderId: "718485621784",
+  appId: "1:718485621784:web:cac547629a659e8cd1c76d",
+  measurementId: "G-MS08DSSK4Y"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
