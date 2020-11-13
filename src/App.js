74079f9400e87eaacafa83d66a7bf16699c0118c
wpenglish <wpenglish27@gmail.com>
2020-11-13 15:01:47 -0500

    Removed Sign up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
 import { Link, Route, Switch } from 'react-router-dom';
+import firebase from 'firebase';
 
 import Lessons from "./components/Lessons"
 import Analytics from "./components/Analytics"
 import Login from './components/Login';
 
+import './App.css';
+
 function App() {
+
+  const LogoutEventListen = () =>{
+    firebase.auth().signOut();
+  }
+
   return (
     <div className="App">
 
@@ -16,6 +23,10 @@ function App() {
       <Link to="/login">
         <a>Takes you to Log in page</a>
       </Link>
+
+      <button id="btnLogout" className="hide" onClick={LogoutEventListen}>
+        Log out
+      </button>
 
       {/* This will switch to different React components while keeping us in this component. If you run this, you'll notice the Links will stay in the window */}
       {/* This will help us keep a header if we need one or if we don't want anything to persist we could put this in index */}
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import firebase from 'firebase';
 import '../css/login.css';
+import {useHistory} from 'react-router-dom';
 
 export default function Login() {
+
+
+    const history = useHistory();
+
+    const redirectToHomePage = useCallback(() => history.push('/'), [history]);
 
     const LoginEventListen = () => {
         const email = document.getElementById("txtEmail").value;
@@ -12,22 +18,11 @@ export default function Login() {
         promise.catch(e => console.log(e.message))
     }
 
-    const SignUpEventListen = () => {
-        const email = document.getElementById("txtEmail").value;
-        const pass = document.getElementById("txtPassword").value;
-        const auth = firebase.auth();
-        const promise = auth.createUserWithEmailAndPassword(email, pass);
-        promise.catch(e => console.log(e.message))
-    }
-
-    const LogoutEventListen = () =>{
-        firebase.auth().signOut();
-    }
-
     firebase.auth().onAuthStateChanged(firebaseUser => {
         if(firebaseUser) {
             console.log(firebaseUser);
             document.getElementById("btnLogout").classList.remove('hide');
+            redirectToHomePage()
         } else {
             console.log('not logged in');
             document.getElementById("btnLogout").classList.add('hide');
@@ -44,13 +39,6 @@ export default function Login() {
                 Log in
             </button>
 
-            <button id="btnSignUp" onClick={SignUpEventListen}>
-                Sign up
-            </button>
-
-            <button id="btnLogout" className="hide" onClick={LogoutEventListen}>
-                Log out
-            </button>
         </div>
     )
 }
