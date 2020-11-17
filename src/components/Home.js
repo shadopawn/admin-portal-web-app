230163f9920e63b9f8c3198ee0111207e17a5698
wpenglish <wpenglish27@gmail.com>
2020-11-17 15:19:35 -0500

    Added Home page layout

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 export default function Home() {
     return (
         <div>
             <h1>Welcome to the Admin Portal</h1>
+            <div className="linkContainer">
+                
+                <Link to="/lesson-editor">
+                    <div className="linkChoice">
+                        <h2>Lessons page </h2>
+                    </div>
+                </Link>
+
+                <Link to="/analytics">
+                    <div className="linkChoice">
+                        <h2>Analytics page </h2>
+                    </div>
+                </Link>
+                
+            </div>
         </div>
     )
 }
