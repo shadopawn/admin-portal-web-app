fe849fde31a8eb03f3db65982b1a6c7f640809f9
wpenglish <wpenglish27@gmail.com>
2020-11-17 16:06:17 -0500

    Update Home.test.js

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { cleanup, render, screen } from '@testing-library/react';
 import Home from '../components/Home';
 import { BrowserRouter } from 'react-router-dom';
 
-test('renders welcome message', () => {
+// The BrowserRouter is required for these test because Home contains "Links" from the same "react-router-dom" 
+// which cannot work without being in a BrowserRouter that is contained in the index element in our app
+
+afterEach(cleanup);
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<BrowserRouter><Home /></BrowserRouter>, div);
+})
+
+it('renders welcome message', () => {
   render(
     <BrowserRouter>
       <Home />
