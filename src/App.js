a2db2111cbddd894b3a105c5e5e64c370b39fc05
Daniel Litt <daniel_litt@comcast.net>
2020-11-29 15:55:19 -0500

    Fix indentation issues

--- src/App.js
+++ src/App.js
@@ -10,50 +10,50 @@ import './css/App.css';
 
 function App() {
 
-  const LogoutEventListen = () =>{
-    firebase.auth().signOut();
-    window.location.href="/admin-portal-web-app";
-  }
+	const LogoutEventListen = () =>{
+	firebase.auth().signOut();
+	window.location.href="/admin-portal-web-app";
+	}
   
-  return (
-    <div className="App">
+	return (
+		<div className="App">
 
-      {/* We should make this a nav component */}
-      <div className="navBar">
-        <Link to="/lesson-editor" className="link" data-testid="lessonLink">
-          <h3>Lessons Editor</h3>
-        </Link>
+			{/* We should make this a nav component */}
+			<div className="navBar">
+				<Link to="/lesson-editor" className="link" data-testid="lessonLink">
+					<h3>Lessons Editor</h3>
+				</Link>
 
-        <Link to="/analytics" className="link">
-          <h3>Analytics Dashboard</h3>
-        </Link>
+				<Link to="/analytics" className="link">
+					<h3>Analytics Dashboard</h3>
+				</Link>
 
-        <Link to="/login" className="link" id="loginLink">
-          <h3>Login Page</h3>
-        </Link>
+				<Link to="/login" className="link" id="loginLink">
+					<h3>Login Page</h3>
+				</Link>
 
-        <button id="btnLogout" className="hide logout" onClick={LogoutEventListen}>
-          Log out
-        </button>
-      </div>
-      
-      <Switch>
-        <PrivateRoute path="/lesson-editor">
-          <LessonEditor />
-        </PrivateRoute>
+				<button id="btnLogout" className="hide logout" onClick={LogoutEventListen}>
+					Log out
+				</button>
+			</div>
+			
+			<Switch>
+				<PrivateRoute path="/lesson-editor">
+					<LessonEditor />
+				</PrivateRoute>
 
-        <PrivateRoute path="/analytics">
-          <Analytics />
-        </PrivateRoute>
+				<PrivateRoute path="/analytics">
+					<Analytics />
+				</PrivateRoute>
 
-        <Route path="/login" component={Login}></Route>
-        <Route path="/admin-portal-web-app" component={Home}></Route>
-        <Route exact path="/">
-          <Redirect to="/admin-portal-web-app" />
-        </Route>
-      </Switch>
-    </div>
-  );
+				<Route path="/login" component={Login}></Route>
+				<Route path="/admin-portal-web-app" component={Home}></Route>
+				<Route exact path="/">
+					<Redirect to="/admin-portal-web-app" />
+				</Route>
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
