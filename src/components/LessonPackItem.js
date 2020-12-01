35a0d197b91a526848fc950a601d83aa8d282964
Daniel Litt <daniel_litt@comcast.net>
2020-12-01 17:47:06 -0500

    Connect delete button on lesson pack list

--- src/components/LessonPackItem.js
+++ src/components/LessonPackItem.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function LessonPackItem({packName}) {
+export default function LessonPackItem({packName, deleteItem}) {
     return (
         <tr>
             <td>{packName}</td>
             <td><button>Edit</button></td>
             <td><button>Publish</button></td>
-            <td><button>Delete</button></td>
+            <td><button onClick={() => deleteItem(packName)} >Delete</button></td>
         </tr>
     )
 }
--- src/components/LessonPacks.js
+++ src/components/LessonPacks.js
@@ -5,8 +5,13 @@ export default function LessonPacks() {
 
     const [packNames, setPackNames] = useState(["Pack 1", "Pack 2", "Pack 3"])
 
+    function handleRemove(name) {
+        const newList = packNames.filter((packName) => packName !== name);
+        setPackNames(newList);
+    }
+
     const lessonPackList = packNames.map((name) =>
-        <LessonPackItem packName={name}/>
+        <LessonPackItem packName={name} deleteItem={() => handleRemove(name)}/>
     );
 
     return (
