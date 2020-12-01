a9bedbdb345fdef122a28c36dc63265d8731d928
wpenglish <wpenglish27@gmail.com>
2020-12-01 15:39:59 -0500

    Added LessonPair for multiple videos

--- src/components/LessonCreationTool.js
+++ src/components/LessonCreationTool.js
@@ -1,31 +1,13 @@
 import React, { useState } from 'react'
-import VideoContainerModal from './VideoContainerModal'
+import LessonPair from './LessonPair';
 
-export default function LessonCreationTool() {
-
-    
-    const [showVideoModal, setshowVideoModal] = useState(false)
-    const [name, setname] = useState("")
-
-    const hideVideoModal = () => {
-        setshowVideoModal(false);
-    }
-
-    const showAVideoModal = () => {
-        setshowVideoModal(true);
-    }
-
-    const getNameOfVideo = (name) => {
-        setname(name);
-        hideVideoModal();
-    }
+export default function LessonCreationTool() { 
     
     return (
         <div>
             <p>Lesson Creation</p>
-            <button onClick={showAVideoModal}>Add Video</button>
-            <VideoContainerModal show={showVideoModal} hide={hideVideoModal} getNameOfVideo={getNameOfVideo} />
-            <h1>  {name} </h1>
+            <LessonPair videoType="Call" />
+            <LessonPair videoType="Analysis" />
         </div>
     )
 }
---
+++ src/components/LessonPair.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import VideoContainerModal from './VideoContainerModal'
+
+export default function LessonPair(props) {
+
+    const [showVideoModal, setshowVideoModal] = useState(false)
+    const [name, setname] = useState("")
+
+    const hideVideoModal = () => {
+        setshowVideoModal(false);
+    }
+
+    const showAVideoModal = () => {
+        setshowVideoModal(true);
+    }
+
+    const getNameOfVideo = (name) => {
+        setname(name);
+        hideVideoModal();
+    }
+
+    return (
+        <div>
+            <VideoContainerModal show={showVideoModal} hide={hideVideoModal} getNameOfVideo={getNameOfVideo} />
+            <div className="videoSelector">
+                <div>
+                    <h2> {props.videoType} video selection:</h2>
+                    <p> {name} </p>
+                </div>
+                <div className='addVideoButton'>
+                <button onClick={showAVideoModal}>Add Video</button>
+                </div>
+                
+                
+            </div>
+        </div>
+    )
+}
