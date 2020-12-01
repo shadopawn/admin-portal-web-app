693a22ae4c7b24de908b6b13048f3ea947a4655e
Daniel Litt <daniel_litt@comcast.net>
2020-12-01 15:34:23 -0500

    Redo styling on navbar to seperate logout and links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,19 +11,19 @@ export default function NavBar() {
 
     return (
         <div className="navBar">
-			<Link to="/lesson-editor" className="link" data-testid="lessonLink">
+			<Link to="/lesson-editor" className="navLink" data-testid="lessonLink">
 				<h3>Lessons Editor</h3>
 			</Link>
 
-			<Link to="/analytics" className="link">
+			<Link to="/analytics" className="navLink">
 				<h3>Analytics Dashboard</h3>
 			</Link>
 
-			<Link to="/login" className="link" id="loginLink">
+			<Link to="/login" className="navLink" id="loginLink">
 				<h3>Login Page</h3>
 			</Link>
 
-			<button id="btnLogout" className="hide logout" onClick={LogoutEventListen}>
+			<button id="btnLogout" className="hide logout navLink" onClick={LogoutEventListen}>
 				Log out
 			</button>
 		</div>
