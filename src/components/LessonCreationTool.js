62477a9ed38b90c8cebd6cdd7955824a59eb4d99
wpenglish <wpenglish27@gmail.com>
2020-12-01 22:32:45 -0500

    Added uploading functionality

--- src/components/LessonCreationTool.js
+++ src/components/LessonCreationTool.js
@@ -1,15 +1,41 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import LessonPair from './LessonPair';
+import firebase from 'firebase';
 
 export default function LessonCreationTool() {
 
     const [lessonTree, setlessonTree] = useState([])
+    const [uploadStatus, setuploadStatus] = useState(false)
+
+    const getLessonTree = () => {
+        setlessonTree([])
+        setuploadStatus(true)
+    }
+
+    const upload = () => {
+        const lessonName = document.getElementById("lessonName").value;
+        for(let i = 0; i < lessonTree.length; i++){
+            firebase.database().ref('lesson_packs/' + lessonName + '/lesson_pair' + i.toString()).update({
+                call_video:lessonTree[i][0],
+                analysis_video:lessonTree[i][1]
+            })
+        }
+    }
     
+    useEffect(() => {
+        if(lessonTree.length !== 0){
+            upload();
+        }
+    }, [lessonTree]);
+
     return (
         <div>
             <p>Lesson Creation</p>
-            <LessonPair changeLessontree={setlessonTree} />
-            {lessonTree}
+            <input type="text" id='lessonName' placeholder="Lesson name"></input>
+            <LessonPair changeLessontree={setlessonTree} upload={uploadStatus} tree={lessonTree} up={setuploadStatus} />
+            <LessonPair changeLessontree={setlessonTree} upload={uploadStatus} tree={lessonTree} up={setuploadStatus} />
+            <button onClick={getLessonTree}>Upload</button>
+            <h1>{lessonTree}</h1>
         </div>
     )
 }
--- src/components/LessonPair.js
+++ src/components/LessonPair.js
@@ -1,22 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import VideoSelectionTool from './VideoSelectionTool';
 
-export default function LessonPair(props) {
+export default function LessonPair(props, tree) {
      
     const [callVideoName, setcallVideoName] = useState("");
     const [analysisVideoName, setanalysisVideoName] = useState("")
+    const [lessonPairTree, setlessonPairTree] = useState([])
 
-    const createLessonJSON = () => {
-        props.changeLessontree([callVideoName, analysisVideoName])
+    useEffect(() => {
+        setlessonPairTree([callVideoName, analysisVideoName])
+    }, [callVideoName, analysisVideoName]);
+
+    useEffect(() => {
+        if (props.upload){
+            props.changeLessontree(tree => [...tree, lessonPairTree])
+            props.up(false)
+        }
         
-    }
+    }, [props.upload])
     
     return (
         <div>
+            <p style={{textAlign:"left"}}>Lesson Pair</p>
             <VideoSelectionTool videoType="Call" changeLessonTree={setcallVideoName} />
             <VideoSelectionTool videoType="Analysis" changeLessonTree={setanalysisVideoName} />
-            <button onClick={createLessonJSON} >Upload</button>
-
         </div>
     )
 }
