70b16065e027e5c70cf1ab85c29fceb26cf86d2e
wpenglish <wpenglish27@gmail.com>
2020-12-10 16:15:47 -0500

    Updated VideoUploader

--- src/components/VideoUploader.js
+++ src/components/VideoUploader.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import firebase from 'firebase';
 
-export default function VideoUploadButton() {
+export default function VideoUploader() {
 
     const fileChangeEvent = (e) => {
         var file = e.target.files[0];
