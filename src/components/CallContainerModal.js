f43edd679033eeafb6fc34239964ee121b50cca3
wpenglish <wpenglish27@gmail.com>
2021-01-20 14:02:39 -0500

    Updated Call Modal

--- src/components/CallContainerModal.js
+++ src/components/CallContainerModal.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import firebase from 'firebase'
 import CallCard from './CallCard';
 
 export default function CallContainerModal(props) {
     const showHideClassName = props.show ? "videoModal display-block" : "videoModal display-none";
+    const [callList, setcallList] = useState([])
+
+    const getFirebaseCalls = () => {
+        var listRef = firebase.storage().ref('basketball_signals/');
+        listRef.listAll().then(function(res) {
+        res.items.forEach(function(itemRef) {
+            setcallList(callList => [...callList, <CallCard key={itemRef.name} name={itemRef.name} handleClick={props.getNameOfCall} />])
+        });
+        }).catch(function(error) {
+            console.log(error);
+        });
+    }
+
+    useEffect(() => {
+        if(props.show){
+            getFirebaseCalls();
+        }
+        else {
+            setcallList([]);
+        }
+    }, [props.show]);
 
     return (
         <div className={showHideClassName}>
             <section className='videoModal-main'>
                 <h2 className='heading'>What call would you like to add?</h2>
-                <CallCard key={"key"} name={"key"}  handleClick={props.getNameOfCall}></CallCard>
-                <CallCard key={"key2"} name={"key2"}  handleClick={props.getNameOfCall}></CallCard>
+                {callList}
                 <button className="standardRedButton" onClick={() => props.hide(false)}>Close</button>
             </section>
         </div>
