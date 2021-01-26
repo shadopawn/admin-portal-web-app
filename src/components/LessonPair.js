6b96618c7b64cd9836969ff441dbd3d522ccec6e
wpenglish <wpenglish27@gmail.com>
2021-01-26 11:12:05 -0500

    Updated styling for lesson pairs

--- src/components/LessonPair.js
+++ src/components/LessonPair.js
@@ -14,32 +14,32 @@ export default function LessonPair({index, lessonPair, rerender, render, deleteP
     return (
         <div>
             <dt className="lessonPairName"><h3>Lesson Pair {index + 1}</h3><button className="standardRedButton" onClick={() => deletePair(index)} data-testid="btnDeletePair">Delete</button></dt>
-                <dd className="videoSelection" onClick={() => handleSelection(index, "call_video")} data-testid="btnCallSelection">
-                    <div className="videoNameDisplay">
+                <dd className="selection" onClick={() => handleSelection(index, "call_video")} data-testid="btnCallSelection">
+                    <div className="nameDisplay">
                         Call Video: {lessonPair.call_video}
                     </div>
                     <VideoSelectionTool index={index} videoType={"call_video"} />
                 </dd>
-                <dd className="videoSelection" onClick={() => handleSelection(index, "analysis_video")} data-testid="btnAnalysisSelection">
-                    <div className="videoNameDisplay">
+                <dd className="selection" onClick={() => handleSelection(index, "analysis_video")} data-testid="btnAnalysisSelection">
+                    <div className="nameDisplay">
                         Analysis Video: {lessonPair.analysis_video}
                     </div>
                     <VideoSelectionTool index={index} videoType={"analysis_video"} />
                 </dd>
-                <dd className="videoSelection" onClick={() => handleSelection(index, "false_call0")}>
-                    <div className="videoNameDisplay">
+                <dd className="selection" onClick={() => handleSelection(index, "false_call0")}>
+                    <div className="nameDisplay">
                         False Call 1: {lessonPair.calls.false_call0}
                     </div>
                     <CallSelectionTool index={index} callType={"false_call0"} callBool={"False"} />
                 </dd>
-                <dd className="videoSelection" onClick={() => handleSelection(index, "false_call1")}>
-                    <div className="videoNameDisplay">
+                <dd className="selection" onClick={() => handleSelection(index, "false_call1")}>
+                    <div className="nameDisplay">
                         False Call 2: {lessonPair.calls.false_call1}
                     </div>
                     <CallSelectionTool index={index} callType={"false_call1"} callBool={"False"} />
                 </dd>
-                <dd className="videoSelection" onClick={() => handleSelection(index, "true_call")}>
-                    <div className="videoNameDisplay">
+                <dd className="selection" onClick={() => handleSelection(index, "true_call")}>
+                    <div className="nameDisplay">
                         True Call: {lessonPair.calls.true_call}
                     </div>
                     <CallSelectionTool index={index} callType={"true_call"} callBool={"True"} />
