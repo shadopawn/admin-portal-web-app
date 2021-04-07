95a82dc2b38be3a28285f346e9ef7df1544dcbb4
wpenglish <wpenglish27@gmail.com>
2021-04-07 11:35:31 -0400

    Updated CallPairSelection to display images of calls

--- src/components/CallPairSelection.js
+++ src/components/CallPairSelection.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import CallSelectionTool from './CallSelectionTool'
+import firebase from 'firebase'
 
 export default function CallPairSelection({ index, callType, handleSelection, lessonPair }) {
 
     const [call, setcall] = useState('Call: ')
     const [callBool, setcallBool] = useState()
+    const [imageURL, setimageURL] = useState()
+
+    const getFirebaseURL = () => {
+        var listRef = firebase.storage().ref('basketball_signals/' + lessonPair.calls[callType]);
+        listRef.getDownloadURL().then(function(url) {
+            setimageURL(url)
+        })
+    }
 
     useEffect(() => {
         if(callType === "false_call0"){
@@ -16,16 +25,16 @@ export default function CallPairSelection({ index, callType, handleSelection, le
         } else {
             setcall("True Call: ")
             setcallBool("True")
-        }// eslint-disable-next-line
-    }, []);
+        }
+        getFirebaseURL()
+        // eslint-disable-next-line
+    }, [lessonPair]);
 
     return (
         <dd className="selection" onClick={() => handleSelection(index, callType)}>
             <div className="nameDisplay">
                 <h4 className="typeDisplay">{call}</h4><br></br>
-                <img width='200' height='200'>
-                
-                </img>
+                <img src={imageURL} width='200' height='200' alt={lessonPair.calls[callType] + " call"}></img>
                 <p>{lessonPair.calls[callType].replace(/\.[^/.]+$/, "")}</p>
                 <CallSelectionTool index={index} callType={callType} callBool={callBool} />
             </div>      
